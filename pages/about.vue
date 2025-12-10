67c9d93381fd8dc042e6e5d19d18d16f21775a27
Bernhard Jordan <brnhrdjrdn@gmail.com>
2025-12-10 15:48:50 +0100

    Setup

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,9 @@
 <template>
   <div>ABOUT ME</div>
 </template>
+
+<script>
+export default {
+  name: 'About',
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
-    <BCard
-      title="Card Title"
-      img-src="https://picsum.photos/id/25/600/300"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 20rem"
-    >
-      <BCardText>
-        Some quick example text to build on the card title and make up the bulk of the card's
-        content.
-      </BCardText>
-      <BButton href="#card-overview" variant="primary">Go somewhere</BButton>
-    </BCard>
     <h1>Index</h1>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'Index',
+}
+</script>
