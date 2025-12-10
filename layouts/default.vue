0c5c8401fee2f76a5bd2925d17be0873899441f5
Bernhard Jordan <brnhrdjrdn@gmail.com>
2025-12-10 18:37:45 +0100

    Setup Build Pipeline

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <header>
-      <h1>Meine App</h1>
+      <h1>Meine App.</h1>
+      <h2>TEST</h2>
     </header>
     <main>
       <slot />
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    ssr: true,
+    nitro: {
+        preset: 'static'
+    },
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
     modules: [
